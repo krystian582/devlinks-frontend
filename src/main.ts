c2bc1d2b0cc7c4768c9ebd645724239de90d3aea
Krystian Koc <krystian582@gmail.com>
2023-10-09 19:46:16 +0200

    Add angular material

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { AppRoutingModule } from './app/app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule)
-  ]
+    importProvidersFrom(AppRoutingModule),
+    provideAnimations()
+]
 })
