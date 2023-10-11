776a865d8c051ee14e1d46a77cb9fbfb7d1eda69
Krystian Koc <krystian582@gmail.com>
2023-10-11 13:57:21 +0200

    Add basic menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    // path: '',
-    // loadComponent: () => import('./')
+    path: '',
+    loadComponent: () => import('./shared/nav/nav.component').then(m => m.NavComponent)
   }
 ];
 
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 @Component({
+  standalone: true,
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
+  ]
 })
 export class NavComponent {
 
